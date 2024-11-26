b10db4b855b95f7ed68c16004a35e57df79158e8
yzg <717818895@qq.com>
2024-11-26 15:25:42 +0800

    mock，element-plus图标，el-container等
    mock，element-plus图标，el-container等

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from "pinia";
 import ElementPlus from "element-plus";
 import App from "./App.vue";
 import router from "./router";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
 if (import.meta.env.VITE_DEV == "true") {
   console.log(import.meta.env.VITE_APP_TITLE);
   import("./mock").then(() => {
@@ -12,13 +14,14 @@ if (import.meta.env.VITE_DEV == "true") {
   });
 } else {
   console.log(import.meta.env.VITE_APP_TITLE);
-  import("./mock").then(() => {
-    startApp();
-  });
+  startApp();
 }
 
 function startApp() {
   const app = createApp(App);
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component);
+  }
   app.use(createPinia());
   app.use(router);
   app.use(ElementPlus);
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,17 @@
-<template></template>
-
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-header>Header</el-header>
+          <el-main>Main
+            <Edit style="width: 1em; height: 1em; margin-right: 8px" />
+            </el-main>
+        </el-container>
+      </el-container>
+    </div>
+  </template>
+  
 
 
 <script lang="ts" setup>
@@ -8,4 +20,5 @@
 
 
 <style lang="scss">
+
 </style>
