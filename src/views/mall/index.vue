d4ce40cb2991a03f88f32a2244adc6ab00e35fc5
叶子叶 <717818895@qq.com>
2024-12-01 16:55:35 +0800

    dev
    dev

--- src/views/mall/index.vue
+++ src/views/mall/index.vue
@@ -1,9 +1,27 @@
 <template>
-    mall
+  <div class="myDiv"></div>
+  <div class="container">
+    <h2>mall</h2>
+  </div>
 </template>
-
-<script lang="ts" setup>  </script>
+<script lang="ts" setup></script>
 
 <style scoped lang="scss">
+// scss的预定义
+$pre-wid: 20px;
+// sass的混入函数，用于混入样式和方法，在scss中，使用@include myMixin;来调用
+@mixin myMixin {
+  background: red;
+}
+.myDiv {
+  @include myMixin;
+  width: $pre-wid;
+}
 
+// sass中嵌套样式，在scss中，使用.container h2{text-decoration-color: blue;}来调用
+.container {
+  h2 {
+    text-decoration-color: blue;
+  }
+}
 </style>
