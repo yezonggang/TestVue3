8788fdd893ee55f85921a1c1a9b9098717c62fb7
yzg <717818895@qq.com>
2024-12-05 10:07:09 +0800

    全局namespace前缀定义
    全局namespace前缀定义

--- src/hooks/useDesign.ts
+++ src/hooks/useDesign.ts
@@ -1,9 +1,13 @@
-// 为啥要在这里引入，因为在<template中使用v前缀定义样式claas名的时候，一般在<style标签中会使用namespace，两者是相对的
-// 所以需要引入这个全局的namespace
-import variables from "@/styles/global.module.scss";
+// 为啥要在这里引入，因为在<template中使用v前缀定义样式claas名的时候，一般在<style标签中会使用namespace，两者是相对的,所以需要引入这个全局的namespace
+// 改成不引入，放在index.css统一全局引入
+// import variables from "@/styles/global.moudle.scss";
 
 export const useDesign = () => {
-  const scssVariables = variables;
+  // 这里直接定义，保持跟global.moudle.scss中一致
+  const scssVariables = {
+    namespace: "v",
+    elNamespace: "el",
+  };
 
   /**
    * 根据给定的类名生成带有命名空间前缀的类名
@@ -18,7 +22,6 @@ export const useDesign = () => {
 
   // 返回包含全局样式变量和前缀类名生成函数的对象
   return {
-    variables: scssVariables,
     getPrefixCls,
   };
 };
